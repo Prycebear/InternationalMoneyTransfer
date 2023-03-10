d2347cb6559e06f70bab84db2d29581b6436c4d9
Prycebear <90221801+Prycebear@users.noreply.github.com>
2023-03-10 17:47:46 +0000

    Set up the project with Security, Postgres and a host of other dependencies.

---
+++ src/test/java/com/example/internationalmoneytransfer/InternationalMoneyTransferApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.internationalmoneytransfer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InternationalMoneyTransferApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
